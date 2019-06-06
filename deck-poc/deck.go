4a76bd0fcc5bf045ac1bf0ae51674f66f17f37c6
Subrata Mazumder <subrata.besu@gmail.com>
2019-06-06 07:47:02 +0530

    l33 tests

--- deck-poc/deck.go
+++ deck-poc/deck.go
@@ -1,37 +1,61 @@
 package main
 
-import ("fmt"
- "io/ioutil"
- "strings")
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
 func newDeck() deck {
-	cards := deck {}
+	cards := deck{}
 
-	cardSuite := [] string {"Spade","Diamond","Heart", "Clubs"}
-	cardValues := [] string {"Ace","One","Two", "Three"}
-	for _, suite:=range cardSuite {
-		for _, value:=range cardValues {
+	cardSuite := []string{"Spade", "Diamond", "Heart", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three"}
+	for _, suite := range cardSuite {
+		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suite)
 		}
 	}
 	return cards
 }
-func (d deck) print(){
+func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
-func deal(d deck, handSize int) (deck, deck){
+func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) toString() string{
+func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-func (d deck) saveToFile(fileName string) error{
-	return ioutil.WriteFile(fileName, []byte(d.toString()),777)
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 777)
+}
+
+func newDeckFromFile(filenam string) deck {
+	bs, err := ioutil.ReadFile(filenam)
+	if err != nil {
+		fmt.Println("Error ---", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- deck-poc/main.go
+++ deck-poc/main.go
@@ -6,11 +6,15 @@ package main
 
 
 func main() {
-	d := newDeck()
+	//d := newDeck()
 	// d.print()
 	// fmt.Println(d.toString())
 	// hand, remainingDeck := deal(d,5)
 	// hand.print()
 	// remainingDeck.print()
-	d.saveToFile("deck.txt")
+	//d.saveToFile("deck.txt")
+	cards := newDeckFromFile("deck.txt")
+	cards.print() 
+	cards.shuffle()
+	cards.print()
 }
